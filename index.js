7022dc4ac924db4f3ccd1397be46857b64845083
zSR01 <marce_lo_2011@hotmail.com>
2022-05-17 11:13:57 -0300

    Upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import chalk from "chalk";
+import fs from 'fs';
+
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+
+    let temp;
+    while((temp = regex.exec(texto)) != null){
+        arrayResultados.push({ [temp[1]] : [temp[2]]})
+    }
+    return(arrayResultados.length === 0 ? "Não há links" : arrayResultados);
+}
+
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, "Caminho errado"));
+}
+async function pegarArquivo(caminhoDoArquivo){
+    const encoding = "utf-8";
+    try{
+        const texto = await fs.promises.readFile(caminhoDoArquivo, 
+            encoding)
+        return(extraiLinks(texto))
+    } catch(erro){
+        trataErro(erro);
+    }
+}
+
+//pegarArquivo('./arquivos/texto.md');
+
+export default pegarArquivo;
